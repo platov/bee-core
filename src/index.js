a767fab10ac93c59ee92f9b6c01efebc020acb73
Alexey Platov <alexey@platov.me>
2016-10-21 13:33:01 +0300

    Added ability to generate Abstract Components Tree

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 class BeeCore {
     constructor() {
-        let self = this, _resolve;
+        let _resolve;
 
         this.isExperienceEditor = false;
         this.mediator = new EventEmitter();
@@ -69,6 +69,90 @@ class BeeCore {
                 throw `[bee-core] Cannot transfer event ${event} on DOM because of no DOM element spcified.`;
             }
         }));
+    }
+
+    /**
+     * Generate Abstract Components tree
+     *
+     * @return {Array} ACT collection
+     * */
+    generateACT() {
+        let chromeElements,
+            flattenChromeElements = [],
+            result = [],
+            _level = 0;
+
+        const OPEN = `[kind='open']`;
+        const CLOSE = `[kind='close']`;
+        const PLACEHOLDER = `code[chrometype='placeholder']`;
+        const RENDERING = `code[chrometype='rendering']`;
+        const SELECTOR = `${PLACEHOLDER}${OPEN}, ${RENDERING}${OPEN}, ${PLACEHOLDER}${CLOSE}, ${RENDERING}${CLOSE}`;
+
+        if ('development' === process.env.NODE_ENV) {
+            console.time('ACT generated');
+        }
+
+        chromeElements = $(document.body).find(SELECTOR);
+
+        if (!chromeElements.length) {
+            return result;
+        }
+
+        // Generate flatten list of open/close chrome rags
+        chromeElements.each(function () {
+            let el = $(this),
+                isPlaceholder = false,
+                isRendering = false;
+
+            if (el.is(OPEN)) {
+                _level++;
+            } else if (el.is(CLOSE)) {
+                _level--;
+                return;
+            }
+
+            if (el.is(PLACEHOLDER)) {
+                isPlaceholder = true;
+            } else if (el.is(RENDERING)) {
+                isRendering = true;
+            }
+
+            if (isPlaceholder || isRendering) {
+                flattenChromeElements.push({
+                    level : _level,
+                    type  : isPlaceholder ? 'placeholder' : 'rendering',
+                    title : el.attr(isPlaceholder ? 'key' : 'hintname'),
+                    chrome: el[0]
+                });
+            }
+        });
+
+        // Convert flatten chrome elements list to Abstract Components Tree
+        (function loop(collection, scopeLevel) {
+            let chrome = flattenChromeElements.shift(),
+                scopeCollection;
+
+            chrome.children = scopeCollection = [];
+
+            // Push me to the scope collection;
+            collection.push(chrome);
+
+            // If next item is child - open new scope
+            if (flattenChromeElements.length && flattenChromeElements[0].level > scopeLevel) {
+                loop(scopeCollection, scopeLevel + 1); // jump in
+            }
+
+            // Go to next sibling
+            if (flattenChromeElements.length && flattenChromeElements[0].level === scopeLevel) {
+                loop(collection, scopeLevel);
+            }
+        })(result, 1);
+
+        if ('development' === process.env.NODE_ENV) {
+            console.timeEnd('ACT generated');
+        }
+
+        return result;
     }
 }
 
