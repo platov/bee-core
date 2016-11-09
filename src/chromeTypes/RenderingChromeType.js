2d5b9796c142b775a881c3c9779b1e3b15c431c2
Alexey Platov <alexey@platov.me>
2016-11-09 08:54:20 +0200

    ACT - WIP

--- src/chromeTypes/RenderingChromeType.js
+++ src/chromeTypes/RenderingChromeType.js
@@ -6,6 +6,8 @@ import beeCore from '../';
 const EVENT_PREFIX = `rendering:`;
 const EVENT_BEFORE_UPDATE = `${EVENT_PREFIX}before-update`;
 const EVENT_UPDATE = `${EVENT_PREFIX}update`;
+const EVENT_BEFORE_END_ACTIVATION = `${EVENT_PREFIX}before-endActivation`;
+const EVENT_END_ACTIVATION = `${EVENT_PREFIX}endActivation`;
 const EVENT_BEFORE_HANDLE_MESSAGE = `${EVENT_PREFIX}before-handleMessage`;
 const EVENT_HANDLE_MESSAGE = `${EVENT_PREFIX}handleMessage`;
 
@@ -16,11 +18,20 @@ let Obj = Sitecore.PageModes.ChromeTypes.Rendering.prototype;
  * Handle Update rendering behavior on rendering updating
  * */
 override('update', Obj,
-    function () {
-        beeCore.mediator.emit(EVENT_BEFORE_UPDATE, this.chrome);
+    function (__shared, data) {
+        beeCore.mediator.emit(EVENT_BEFORE_UPDATE, this.chrome, data);
     },
-    function () {
-        beeCore.mediator.emit(EVENT_UPDATE, this.chrome);
+    function (__shared, data) {
+        beeCore.mediator.emit(EVENT_UPDATE, this.chrome, data);
+    }
+);
+
+override('_endActivation', Obj,
+    function (__shared, data) {
+        beeCore.mediator.emit(EVENT_BEFORE_END_ACTIVATION, this.chrome);
+    },
+    function (__shared, data) {
+        beeCore.mediator.emit(EVENT_END_ACTIVATION, this.chrome);
     }
 );
 
